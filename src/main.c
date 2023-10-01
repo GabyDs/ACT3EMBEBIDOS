d0ba9e84fe480b49e4fefb480bf2d9623061b07a
Gabriel Da Silva <gabrields@pop-os.localdomain>
2023-09-30 23:53:17 -0300

    agregado timer para alarma

--- src/main.c
+++ src/main.c
@@ -4,22 +4,22 @@
 
 #define F_CPU 16000000L // Frecuencia del atmega328p
 
-#define ALARMA PORTB0
+#define ALARMA PORTD7
 
-#define tiempo_rebote 10 // ms
+#define tiempo_rebote 6 // ms
 
 #define OCR0A_display 199
+#define OCR0B_alarma 31250
 
-enum DISPLAY
-{
-    Q1 = PORTC0,
-    Q2 = PORTC1,
-    Q3 = PORT2
-};
+#define Q1 PORTC0
+#define Q2 PORTC1
+#define Q3 PORTC2
 
-volatile int habilitado = -1; // 1 -> habilitado; -1 -> deshabilitado
-volatile int umbral = 50;     // umbral -> 50-400
-volatile int contador = 45;
+volatile uint8_t habilitado = -1; // 1 -> habilitado; -1 -> deshabilitado
+volatile uint16_t umbral = 50;    // umbral -> 50-400
+volatile uint16_t contador = 125;
+uint16_t cuenta = 48;
+volatile uint8_t contador_timer1 = 0;
 
 uint8_t estado_display = Q1;
 uint8_t unidad, decena, centena = 0;
@@ -40,17 +40,34 @@ ISR(TIMER0_COMPA_vect)
         ciclar_display(umbral);
 }
 
+ISR(TIMER1_COMPA_vect)
+{
+    contador_timer1++;
+    if (contador_timer1 >= 5)
+    {
+        PORTD &= ~(1 << ALARMA);               // Apaga alarma
+        TCCR1B &= ~(1 << CS12) & ~(1 << CS10); // Detiene conteo
+        contador_timer1 = 0;
+    }
+}
+
 int main(void)
 {
-    // Timer
+    // Timer0
     TCCR0A = (1 << WGM01);              // CTC mode
     OCR0A = OCR0A_display;              // TOP -> aprox 15ms
     TCCR0B = (1 << CS02) | (1 << CS00); // clk_I/O /1024 (from prescaler)
     TIMSK0 = (1 << OCIE0A);             // habilita la interrupcion por comparacion
 
+    // Timer 1
+    TCCR1B = (1 << WGM12); // Modo CTC
+    OCR1A = OCR0B_alarma;
+    TIMSK1 = (1 << OCIE1A); // Interrupción por igualación TCNT1 == OCR1A
+
     sei();
 
     // Entradas
+    // DDRD = (1 << ALARMA);
     PORTD = (1 << PORTD2) | (1 << PORTD3) | (1 << PORTD4);
 
     // Salidas
@@ -61,46 +78,64 @@ int main(void)
 
     while (1)
     {
-        estadoPD4 = (PIND & (1 << PIND4));
-        if (estadoPD4 != ultimo_estadoPD4)
-        {
-            if (!estado_display)
-            {
-                _delay_ms(10 * tiempo_rebote);
-                if (!(PIND & (1 << PIND4)))
-                {
-                    if (habilitado == 1) // Incrementar el contador si esta habilitado
-                        contador += 1;
-                }
-            }
-        }
-
-        estadoPD3 = (PIND & (1 << PIND3));
-        if (estadoPD3 != ultimo_estadoPD3)
-        {
-            if (!estadoPD3)
-            {
-                _delay_ms(10 * tiempo_rebote);
-                if (!(PIND & (1 << PIND3)))
-                    habilitado *= -1; // Habilitar el conteo
-            }
-        }
 
         estadoPD2 = (PIND & (1 << PIND2));
         if (estadoPD2 != ultimo_estadoPD2) // es decir que cambio el estado
         {
             if (!estadoPD2)
             {
-                _delay_ms(10 * tiempo_rebote);
+                _delay_ms(tiempo_rebote);
                 if (!(PIND & (1 << PIND2)))
                 {
-                    umbral += 1; // Incrementa el umbral
+                    umbral++; // Incrementa el umbral
                     contador = 0;
 
                     if (umbral > 400)
                         umbral = 50;
                 }
             }
+        }
+        ultimo_estadoPD2 = estadoPD2;
+
+        estadoPD3 = (PIND & (1 << PIND3));
+        if (estadoPD3 != ultimo_estadoPD3)
+        {
+            if (!estadoPD3)
+            {
+                _delay_ms(tiempo_rebote);
+                if (!(PIND & (1 << PIND3)))
+                    habilitado *= -1; // Habilitar el conteo
+            }
+        }
+        ultimo_estadoPD3 = estadoPD3;
+
+        estadoPD4 = (PIND & (1 << PIND4));
+        if (estadoPD4 != ultimo_estadoPD4)
+        {
+            if (!estado_display)
+            {
+                _delay_ms(tiempo_rebote);
+                if (!(PIND & (1 << PIND4)))
+                {
+                    if (habilitado) // Incrementar el contador si esta habilitado
+                    {
+                        contador++;
+                        cuenta++;
+                    }
+                    if (contador > 999)
+                        contador = 0;
+                }
+            }
+        }
+        ultimo_estadoPD4 = estadoPD4;
+
+        if ((cuenta >= umbral) && !(PINC & (1 << ALARMA)))
+        {
+            cuenta = 0;
+            // si contador por encima de umbral y alarma no está encendida
+            // encender alarma por 10 segundos
+            PORTD |= (1 << ALARMA);
+            TCCR1B |= (1 << CS12) | (1 << CS10); // Prescaler 1024. Inicia el conteo
         }
     }
     return 0;
